350fc01bdc6630daca90a80b625218afecc61509
A7medK7aled1 <akhaledgamal7@gmail.com>
2025-03-04 20:03:15 +0200

    add custom app bar to home

--- lib/features/splash/presentation/views_model/views/widget/spalsh_view_body.dart
+++ lib/features/splash/presentation/views_model/views/widget/spalsh_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/constant.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views_model/views/widget/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SpalshViewBody extends StatefulWidget {
   const SpalshViewBody({super.key});
@@ -17,16 +20,8 @@ class _SpalshViewBodyState extends State<SpalshViewBody>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-    slidingAnimation = Tween<Offset>(
-      begin: Offset(0, 2),
-      end: Offset(0, 0),
-    ).animate(animationController);
-
-    animationController.forward(); // Start the animation
+    initSlidingAnimation(); // Start the animation
+    navigateToHome();
   }
 
   @override
@@ -47,5 +42,25 @@ class _SpalshViewBodyState extends State<SpalshViewBody>
         SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 2),
+      end: Offset(0, 0),
+    ).animate(animationController);
+
+    animationController.forward(); // Start the animation
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.leftToRight, duration: kTransitionDurations);
+    });
   }
 }
