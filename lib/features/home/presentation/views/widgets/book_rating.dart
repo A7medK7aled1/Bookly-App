2deec6ac628c4531d84073428e4c70ff8380aacf
A7medK7aled1 <akhaledgamal7@gmail.com>
2025-03-06 20:32:26 +0200

    add BookRating widget and integrate it into BestSellerListViewItem

---
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -0,0 +1,28 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BookRating extends StatelessWidget {
+  const BookRating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Icon(
+          FontAwesomeIcons.solidStar,
+          color: Colors.yellow,
+          size: 16,
+        ),
+        SizedBox(width: 6.3),
+        Text('4.8', style: Styles.textStyle16),
+        SizedBox(width: 5),
+        Text(
+          '(254)',
+          style: Styles.textStyle14.copyWith(color: Colors.grey),
+        )
+      ],
+    );
+  }
+}
