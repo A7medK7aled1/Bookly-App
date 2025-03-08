3a01a10b9e5ab53fa0a82c0b4c5a89ff40c1b94d
A7medK7aled1 <akhaledgamal7@gmail.com>
2025-03-08 16:39:12 +0200

    refactor BookDetailsViewBody to use CustomScrollView for improved layout; maintain existing features and add spacing adjustments

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -12,44 +12,53 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.257),
-            child: FeaturedListViewItem(),
+
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              children: [
+                const CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * 0.257),
+                  child: FeaturedListViewItem(),
+                ),
+                const SizedBox(height: 20),
+                Text('The Jungle Book',
+                    style: Styles.textStyle20.copyWith(fontSize: 25)),
+                const SizedBox(height: 6),
+                Opacity(
+                  opacity: 0.7,
+                  child: Text('Rudyard Kipling',
+                      style: Styles.textStyle16.copyWith(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500,
+                      )),
+                ),
+                const SizedBox(height: 0),
+                BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(height: 8),
+                BooksAction(),
+                Expanded(child: const SizedBox(height: 20)),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('You can also like',
+                      style: Styles.textStyle14
+                          .copyWith(fontWeight: FontWeight.w600)),
+                ),
+                const SizedBox(height: 20),
+                SimilarBookListView(),
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
-          const SizedBox(height: 20),
-          Text('The Jungle Book',
-              style: Styles.textStyle20.copyWith(fontSize: 25)),
-          const SizedBox(height: 6),
-          Opacity(
-            opacity: 0.7,
-            child: Text('Rudyard Kipling',
-                style: Styles.textStyle16.copyWith(
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w500,
-                )),
-          ),
-          const SizedBox(height: 0),
-          BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(height: 8),
-          BooksAction(),
-          const SizedBox(height: 20),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text('You can also like',
-                style:
-                    Styles.textStyle14.copyWith(fontWeight: FontWeight.w600)),
-          ),
-          const SizedBox(height: 20),
-          SimilarBookListView(),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
