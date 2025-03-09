3057f4b43bb6cefbfb8265407d128610d24d7b1e
A7medK7aled1 <akhaledgamal7@gmail.com>
2025-03-09 23:03:26 +0200

    implement NewsedBooksCubit with fetching logic and state management for new set of featured books

--- lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
 
 class NewsedBooksCubit extends Cubit<NewsetBooksState> {
-  NewsedBooksCubit() : super(NewsetBooksInitial());
+  NewsedBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => emit(NewsetBooksFailure(failure.toString())),
+      (books) => emit(NewsetBooksSuccess(books)),
+    );
+  }
 }
