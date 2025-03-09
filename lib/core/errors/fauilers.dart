470afe8566dbd54ccbe224f306c3bdbae9f3d527
A7medK7aled1 <akhaledgamal7@gmail.com>
2025-03-09 22:33:51 +0200

    implement error handling for API interactions; enhance Fauilers class for detailed error messages

--- lib/core/errors/fauilers.dart
+++ lib/core/errors/fauilers.dart
@@ -1,3 +1,48 @@
-abstract class Fauilers {}
+import 'package:dio/dio.dart';
 
-class ServerFauiler extends Fauilers {}
+abstract class Fauilers {
+  final String errMessage;
+
+  Fauilers(this.errMessage);
+}
+
+class ServerFauiler extends Fauilers {
+  ServerFauiler(super.errMessage);
+
+  factory ServerFauiler.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFauiler('Connection Timeout with Api Server');
+      case DioExceptionType.sendTimeout:
+        return ServerFauiler('Send Timeout with Api Server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFauiler('Receive Timeout with Api Server');
+      case DioExceptionType.badCertificate:
+        return ServerFauiler('Bad Certificate Error with Api Server');
+      case DioExceptionType.badResponse:
+        return ServerFauiler.fromResponse(
+            dioException.response!.statusCode!, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFauiler('Request Cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFauiler('Connection Error with Api Server');
+      case DioExceptionType.unknown:
+        if (dioException.message!.contains('SocketException')) {
+          return ServerFauiler('No Internet Connection');
+        }
+        return ServerFauiler('Unknown Error');
+    }
+  }
+
+  factory ServerFauiler.fromResponse(int statysCode, dynamic response) {
+    if (statysCode == 400 || statysCode == 401 || statysCode == 403) {
+      return ServerFauiler(response['error']['message']);
+    } else if (statysCode == 404) {
+      return ServerFauiler('Your requset not found, please try again');
+    } else if (statysCode == 500) {
+      return ServerFauiler('Internal Server Error, please try again later');
+    } else {
+      return ServerFauiler('Oops! Something went wrong');
+    }
+  }
+}
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,12 +1,36 @@
-import 'package:bookly_app/core/errors/fauilers.dart';
-import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
-import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
 import 'package:dartz/dartz.dart';
 
+import 'package:bookly_app/core/errors/fauilers.dart';
+import 'package:bookly_app/core/utils/api_serves.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:dio/dio.dart';
+
 class HomeRepoImpl implements HomeRepo {
+  final ApiServes apiServes;
+  HomeRepoImpl({
+    required this.apiServes,
+  });
   @override
-  Future<Either<Fauilers, List<BookModel>>> fetchBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Fauilers, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      var data = await apiServes.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&sorting=newest &q=subject:programming');
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return Right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFauiler.fromDioException(e));
+      }
+      return Left(ServerFauiler(e.toString()));
+    }
   }
 
   @override
